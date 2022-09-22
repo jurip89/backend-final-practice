2bad6da39f3fb9bdb13c658a5d75e4d3521dd5d1
jurip89 <juri@Barbaras-iMac.local>
2022-09-22 20:37:15 +0200

    made my life easier

--- routers/spaces.js
+++ routers/spaces.js
@@ -18,6 +18,7 @@ router.get('/',async(req,res,next)=>{
 router.post('/',async(req,res,next)=>{
     try {
         const newOne = await Space.create(req.body);
+         
         res.send(newOne)
     } catch (error) {
         next(error)
@@ -48,16 +49,19 @@ router.patch('/:id',authMiddleware,async(req,res,next)=>{
     }
 })
 
-router.get('/:id/stories',authMiddleware,async(req,res,next)=>{
+router.get('/:id/stories',async(req,res,next)=>{
     const {id} = req.params;
     try {
+        const space = await Space.findByPk(id);
+        !space && res.status(404).send('Nothing here Bro!')
      const stories=await Story.findAll({where:{spaceId :id}})
-     res.send(stories)
+     stories.lenght < 1 && res.send({space,stories:[]})
+     res.send({space,stories})
     } catch (error) {
         next(error)
     }
 })
-router.post('/:id/stories',async(req,res,next)=>{
+router.post('/:id/stories',authMiddleware,async(req,res,next)=>{
     const {id} = req.params;
     try {
         const newOne = await Story.create({...req.body,spaceId:id})
@@ -67,7 +71,7 @@ router.post('/:id/stories',async(req,res,next)=>{
     }
 })
 
-router.delete('/:id/stories/:storyId',async(req,res,next)=>{
+router.delete('/:id/stories/:storyId',authMiddleware,async(req,res,next)=>{
     const {id,storyId} =req.params;
     try {
         const theOne = await Story.findByPk(storyId);
